a329d8c5e5f089cc822244ee4b04c6e6dd2d3624
Lance Jonn Romanoff <ljr@ljonn.com>
2020-07-23 19:04:24 -0700

    Adjusting max viewport media query

--- source/js/dctsdk-slider.js
+++ source/js/dctsdk-slider.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
       // variableHeight: true,
       responsive: [
         {
-          breakpoint: 1440,
+          breakpoint: 1024,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -52,7 +52,7 @@ $(document).ready(function(){
       slidesToScroll: 3,
       responsive: [
         {
-          breakpoint: 1440,
+          breakpoint: 1024,
           settings: {
             slidesToShow: 3,
             slidesToScroll: 1,
@@ -95,7 +95,7 @@ $(document).ready(function(){
   //    slidesToScroll: 1,
   //     responsive: [
   //       {
-  //         breakpoint: 1440,
+  //         breakpoint: 1024,
   //         settings: {
   //           slidesToShow: 3,
   //           slidesToScroll: 3,
